7caedae97ba4c5ca245ff184bab7fdf432a833af
Mikecoder17 <mikwiatkowski111100@gmail.com>
2023-06-18 21:52:48 +0200

    corrected bugs

--- languages.js
+++ languages.js
@@ -1,34 +1,23 @@
-import("./languages_data")
-
-// console.log(data.authotdesc)
-
 let langs = document.querySelector(".langs"),
      link = document.querySelectorAll(".langs a"),
      title = document.querySelector(".title"),
-     descr = document.querySelector(".description");
-     ticmenu = document.querySelector(".ticmenu");
+     descr = document.querySelector(".description"),
+     ticmenu = document.querySelector(".ticmenu"),
+     irysymenu = document.querySelector(".irysymenu"),
+     bajecznemenu = document.querySelector(".bajecznemenu"),
+     shopbutton = document.querySelector(".shopbutton"),
+     descbutton = document.querySelector(".descbutton"),
+     shoptitle = document.querySelector(".shoptitle"),
+     shopdesc = document.querySelector(".shopdesc"),
+     irysydesc = document.querySelector(".irysydesc"),
+     bajecznedesc = document.querySelector(".bajecznedesc"),
+     michalkidesc = document.querySelector(".michalkidesc"),
+     gametitle = document.querySelector(".gametitle"),
+     movedesc = document.querySelector(".movedesc"),
+     playwithfriend = document.querySelector(".playwithfriend"),
+     playwithcomputer = document.querySelector(".playwithcomputer"),
+     computerfirst = document.querySelector(".computerfirst")
 
-  
-     irysymenu = document.querySelector(".irysymenu");
-     bajecznemenu = document.querySelector(".bajecznemenu");
-     shopbutton = document.querySelector(".shopbutton");
-     descbutton = document.querySelector(".descbutton");
-
-     shoptitle = document.querySelector(".shoptitle");
-     shopdesc = document.querySelector(".shopdesc");
-
-
-     irysydesc = document.querySelector(".irysydesc");
-     bajecznedesc = document.querySelector(".bajecznedesc");
-     michalkidesc = document.querySelector(".michalkidesc");
-
-
-
-     movedesc = document.querySelector(".movedesc");
-     playwithfriend = document.querySelector(".playwithfriend");
-     playwithcomputer = document.querySelector(".playwithcomputer");
-     computerfirst = document.querySelector(".computerfirst");
-     authordesc = document.querySelector(".authotdesc");
 
 
 
@@ -42,72 +31,20 @@ link.forEach(el=>{
           title.textContent = data[attr].title
           descr.textContent = data[attr].description
           ticmenu.textContent = data[attr].ticmenu
-
           irysymenu.textContent = data[attr].irysymenu
           bajecznemenu.textContent = data[attr].bajecznemenu
           shopbutton.textContent = data[attr].shopbutton
-          descbutton.textContent = data[attr].descbutton
-
-          
+          descbutton.textContent = data[attr].descbutton   
           shoptitle.textContent = data[attr].shoptitle
           shopdesc.textContent = data[attr].shopdesc
-
           irysydesc.textContent = data[attr].irysydesc
           bajecznedesc.textContent = data[attr].bajecznedesc
-          michalkidesc.textContent = data[attr].michalkidesc
-          
+          michalkidesc.textContent = data[attr].michalkidesc 
+          gametitle.textContent = data[attr].gametitle
           movedesc.textContent = data[attr].movedesc
           playwithfriend.textContent = data[attr].playwithfriend
           playwithcomputer.textContent = data[attr].playwithcomputer
-          computerfirst.textContent = data[attr].computerfirst
-          authordesc.textContent = data[attr].authotdesc
-     
+          computerfirst.textContent = data[attr].computerfirst     
 
      })
 })
-
-// let data = {
-//      polish: {
-//           title: "Irysy - sklep z cukierkami",
-//           description: "Chyba każdy pamięta krówkę ciągutkę albo niesamowity, ponadczasowy i kultowy smak irysów...",
-//           ticmenu: "Kółko i krzyżyk",
-//           irysymenu: "Irysy",
-//           bajecznemenu: "Bajeczne",
-//           shopbutton:"Przejdź do sklepu",
-//           descbutton:"O naszych cukierkach",
-//           shoptitle:"Nasz sklep z cukierkami",
-//           shopdesc:"Raj dla wszystkich miłośników słodyczy - niepowtarzalny sklep z cukierkami. Oferujemy szeroki wybór pysznych i kolorowych cukierków, od klasycznych smaków po unikalne i egzotyczne kompozycje.",
-//           irysydesc:"Irysy to kultowe polskie cukierki o niezwykłym smaku i teksturze. Są wykonane z delikatnej masy karmelowej, pokrytej czekoladą. Ich unikalna kombinacja słodyczy i lekkości sprawia, że są nieodłącznym elementem wspomnień dzieciństwa i ulubionym smakołykiem wielu osób.",
-//           bajecznedesc:"Bajeczne to wyjątkowe cukierki o magicznym smaku. Ich kolorowe i fantazyjne wzory wzbudzają wyobraźnię, a ich słodki i owocowy smak przywołuje uczucie czarowniczych opowieści. Bajczne są nie tylko smaczne, ale także pięknie prezentują się jako dekoracja na słodkie stoły i przyjęcia.",
-//           michalkidesc:"Michałki to popularne polskie łakocie o intensywnym orzechowym smaku. Są wykonane z karmelu i prażonych orzechów, tworząc niezwykle chrupiącą i aromatyczną kompozycję. Ich unikalna tekstura i doskonałe połączenie słodkości z orzechowym posmakiem czynią je niezwykle rozkosznym przysmakiem.",
-          
-//           movedesc:"Ruch gracza",
-//           playwithfriend:"Graj ze znajomym",  
-//           playwithcomputer:"Graj z komputerem",
-//           computerfirst: "Komputer zaczyna",
-//           authordesc:"Autor: Mikecoder", 
-//      },
-     
-//      english: {
-//           title: "Irysy - sweetshop",
-//           description: "I think everyone remembers the cow caramel or the amazing, timeless, and iconic taste of irises...",
-//           ticmenu: "TicTacToe",
-//           irysymenu: "Irises",
-//           bajecznemenu: "Bajeczne",
-//           shopbutton: "Go to the store",
-//           descbutton: "About our candies",
-//           shoptitle: "Our candy store",
-//           shopdesc:"A paradise for all candy lovers - a unique candy store. We offer a wide selection of delicious and colorful candies, ranging from classic flavors to unique and exotic combinations.",
-         
-//           irysydesc: "Irises are iconic Polish candies with a remarkable taste and texture. They are made of delicate caramel mass covered in chocolate. Their unique combination of sweetness and lightness makes them a nostalgic childhood treat and a favorite indulgence for many.",
-//           bajecznedesc:"Bajeczne candies are unique sweets with a magical flavor. Their colorful and imaginative patterns spark the imagination, while their sweet and fruity taste evokes the feeling of enchanting stories. Bajeczne candies are not only delicious but also make beautiful decorations for sweet tables and parties.",
-//           michalkidesc:"Michałki are popular Polish sweets with an intense nutty flavor. They are made of caramel and roasted nuts, creating an incredibly crunchy and aromatic composition. Their unique texture and perfect balance of sweetness with a nutty aftertaste make them a delightful treat.",
-         
-//           movedesc: "Player's move",
-//           playwithfriend: "Play with a friend",
-//           playwithcomputer: "Play with the computer",
-//           computerfirst: "Computer starts",
-//           authordesc: "Author: Mikecoder",
-//      },
-     
-// }
--- languages_data.js
+++ languages_data.js
@@ -11,15 +11,32 @@ const data = {
          shopdesc:"Raj dla wszystkich miłośników słodyczy - niepowtarzalny sklep z cukierkami. Oferujemy szeroki wybór pysznych i kolorowych cukierków, od klasycznych smaków po unikalne i egzotyczne kompozycje.",
          irysydesc:"Irysy to kultowe polskie cukierki o niezwykłym smaku i teksturze. Są wykonane z delikatnej masy karmelowej, pokrytej czekoladą. Ich unikalna kombinacja słodyczy i lekkości sprawia, że są nieodłącznym elementem wspomnień dzieciństwa i ulubionym smakołykiem wielu osób.",
          bajecznedesc:"Bajeczne to wyjątkowe cukierki o magicznym smaku. Ich kolorowe i fantazyjne wzory wzbudzają wyobraźnię, a ich słodki i owocowy smak przywołuje uczucie czarowniczych opowieści. Bajczne są nie tylko smaczne, ale także pięknie prezentują się jako dekoracja na słodkie stoły i przyjęcia.",
-         michalkidesc:"Michałki to popularne polskie łakocie o intensywnym orzechowym smaku. Są wykonane z karmelu i prażonych orzechów, tworząc niezwykle chrupiącą i aromatyczną kompozycję. Ich unikalna tekstura i doskonałe połączenie słodkości z orzechowym posmakiem czynią je niezwykle rozkosznym przysmakiem.",
-         
+         michalkidesc:"Michałki to popularne polskie łakocie o intensywnym orzechowym smaku. Są wykonane z karmelu i prażonych orzechów, tworząc niezwykle chrupiącą i aromatyczną kompozycję. Ich unikalna tekstura i doskonałe połączenie słodkości z orzechowym posmakiem czynią je niezwykle rozkosznym przysmakiem.",      
+         gametitle:"Zagraj w kółko i krzyżyk",
          movedesc:"Ruch gracza",
          playwithfriend:"Graj ze znajomym",  
          playwithcomputer:"Graj z komputerem",
          computerfirst: "Komputer zaczyna",
-         authordesc:"Autor: Mikecoder", 
     },
-    
+    slovenian: {
+        title: "Irysy - slaščičarna",
+        description: "Mislim, da se vsi spomnimo kravjega karamela ali neverjetnega, brezčasnega in ikoničnega okusa irisov...",
+        ticmenu: "Igra križec krožec",
+        irysymenu: "Irisi",
+        bajecznemenu: "Bajčni",
+        shopbutton: "Pojdi v trgovino",
+        descbutton: "O naših sladicah",
+        shoptitle: "Naša slaščičarna",
+        shopdesc: "Raj za vse ljubitelje sladkarij - edinstvena slaščičarna. Ponujamo širok izbor okusnih in barvitih sladkarij, ki segajo od klasičnih okusov do edinstvenih in eksotičnih kombinacij.",
+        irysydesc: "Irisi so ikonične poljske sladkarije z izjemnim okusom in teksturo. Narejeni so iz nežne karamelne mase, prekrite s čokolado. Njihova edinstvena kombinacija sladkosti in lahkosti jih naredi nostalgijo iz otroštva in priljubljeno razvajanje za mnoge.",
+        bajecznedesc: "Bajčne sladkarije so edinstvene sladkarije s čarobnim okusom. Njihovi barviti in domišljijski vzorci vzbujajo domišljijo, medtem ko njihov sladek in sadni okus pričara občutek očarljivih zgodb. Bajčne sladkarije niso samo okusne, ampak so tudi lep okras za sladke mize in zabave.",
+        michalkidesc: "Michałki so priljubljene poljske sladkarije z intenzivnim oreščkovim okusom. Narejeni so iz karamela in praženih oreščkov, kar ustvarja izjemno hrustljavo in aromatično sestavo. Njihova edinstvena tekstura in popolno ravnovesje sladkosti s oreščkovim pookusom jih naredita čudovito sladico.",
+        gametitle:"Igrajmo igro križec krožec",
+        movedesc: "Poteza igralca",
+        playwithfriend: "Igraj z prijateljem",
+        playwithcomputer: "Igraj s računalnikom",
+        computerfirst: "Računalnik začne",
+        },
     english: {
          title: "Irysy - sweetshop",
          description: "I think everyone remembers the cow caramel or the amazing, timeless, and iconic taste of irises...",
@@ -29,18 +46,15 @@ const data = {
          shopbutton: "Go to the store",
          descbutton: "About our candies",
          shoptitle: "Our candy store",
-         shopdesc:"A paradise for all candy lovers - a unique candy store. We offer a wide selection of delicious and colorful candies, ranging from classic flavors to unique and exotic combinations.",
-        
+         shopdesc:"A paradise for all candy lovers - a unique candy store. We offer a wide selection of delicious and colorful candies, ranging from classic flavors to unique and exotic combinations.",  
          irysydesc: "Irises are iconic Polish candies with a remarkable taste and texture. They are made of delicate caramel mass covered in chocolate. Their unique combination of sweetness and lightness makes them a nostalgic childhood treat and a favorite indulgence for many.",
          bajecznedesc:"Bajeczne candies are unique sweets with a magical flavor. Their colorful and imaginative patterns spark the imagination, while their sweet and fruity taste evokes the feeling of enchanting stories. Bajeczne candies are not only delicious but also make beautiful decorations for sweet tables and parties.",
          michalkidesc:"Michałki are popular Polish sweets with an intense nutty flavor. They are made of caramel and roasted nuts, creating an incredibly crunchy and aromatic composition. Their unique texture and perfect balance of sweetness with a nutty aftertaste make them a delightful treat.",
-        
+         gametitle:"Let's play tictactoe game",
          movedesc: "Player's move",
          playwithfriend: "Play with a friend",
          playwithcomputer: "Play with the computer",
          computerfirst: "Computer starts",
-         authordesc: "Author: Mikecoder",
     },
     
 }
-export {data}
